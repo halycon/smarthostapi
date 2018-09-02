a8c261fa41aebc72921d6716bf4c62436f04dd1a
volkancetin <vncetin@gmail.com>
2018-09-02 17:54:56 +0300

    initial commit

---
+++ src/main/java/com/halycon/smarthostapi/SmarthostapiApplication.java
@@ -0,0 +1,12 @@
+package com.halycon.smarthostapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthostapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthostapiApplication.class, args);
+	}
+}
