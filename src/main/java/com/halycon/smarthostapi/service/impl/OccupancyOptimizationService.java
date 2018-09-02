89d2b458fcfa6277551740c92727e82b2e98d474
volkancetin <vncetin@gmail.com>
2018-09-02 19:17:48 +0300

    Simple request validation and sorting request parameters before optimization.

--- src/main/java/com/halycon/smarthostapi/service/impl/OccupancyOptimizationService.java
+++ src/main/java/com/halycon/smarthostapi/service/impl/OccupancyOptimizationService.java
@@ -1,11 +1,15 @@
 package com.halycon.smarthostapi.service.impl;
 
+import com.halycon.smarthostapi.domain.RoomStock;
 import com.halycon.smarthostapi.domain.api.OccupancyOptimizationRequest;
 import com.halycon.smarthostapi.domain.api.OccupancyOptimizationResponse;
 import com.halycon.smarthostapi.service.IOptimizationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service("OccupancyOptimizationService")
 public class OccupancyOptimizationService implements IOptimizationService<OccupancyOptimizationRequest, OccupancyOptimizationResponse> {
@@ -15,7 +19,31 @@ public class OccupancyOptimizationService implements IOptimizationService<Occupa
     @Override
     public OccupancyOptimizationResponse optimize(OccupancyOptimizationRequest occupancyOptimizationRequest) throws Exception {
 
-        return null;
+        validateOccupancyOptimizationRequest(occupancyOptimizationRequest);
+
+        OccupancyOptimizationResponse response = new OccupancyOptimizationResponse();
+
+        Arrays.parallelSort(occupancyOptimizationRequest.getPrices());
+
+        occupancyOptimizationRequest.getRoomStockList().sort(
+                (RoomStock r1, RoomStock r2) -> r2.getMinPrice().compareTo(r1.getMinPrice()));
+
+        return response;
     }
 
+    public void validateOccupancyOptimizationRequest(OccupancyOptimizationRequest occupancyOptimizationRequest) throws Exception{
+        if(occupancyOptimizationRequest.getRoomStockList()==null || occupancyOptimizationRequest.getRoomStockList().size()==0)
+            throw new IllegalArgumentException("RoomStock definitions missing!");
+        if(occupancyOptimizationRequest.getPrices()==null || occupancyOptimizationRequest.getPrices().length==0)
+            throw new IllegalArgumentException("Prices missing!");
+
+        validateRoomStockDefinitions(occupancyOptimizationRequest.getRoomStockList());
+    }
+
+    public void validateRoomStockDefinitions(List<RoomStock> roomStockList){
+        for(RoomStock roomStock : roomStockList) {
+            if(roomStock.getRoomType()==null)
+                throw new IllegalArgumentException("RoomType not defined in RoomStock :: "+roomStock.toString());
+        }
+    }
 }
