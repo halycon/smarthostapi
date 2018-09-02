7793edaa87e11469387952e7eed7536c280f87f7
volkancetin <vncetin@gmail.com>
2018-09-02 22:42:57 +0300

    swagger documentation and README file generated.

---
+++ README.md
@@ -0,0 +1,26 @@
+## diff-service
+Assignment project for Smart Host company.
+Based on room definitions and prices, optimized total allocation 
+and revenue will be returned.
+
+## Operations
+(POST) /getOptimizedOccupancySummary  : operation for getting optimized allocations and revenues.
+
+## Tech/Framework used
+Spring Boot, Swagger
+
+<b>Built with</b>
+- [Maven](https://maven.apache.org/)
+
+## Installation
+
+Maven have to be installed on local
+
+"maven test" for testing
+
+"mvn spring-boot:run" for getting service up 
+
+## Documentation
+http://localhost:8080/swagger-ui.html#
+      
+© [Volkan Cetin]()
--- src/main/java/com/halycon/smarthostapi/controller/InsightsController.java
+++ src/main/java/com/halycon/smarthostapi/controller/InsightsController.java
@@ -3,6 +3,8 @@ package com.halycon.smarthostapi.controller;
 import com.halycon.smarthostapi.domain.api.OccupancyOptimizationRequest;
 import com.halycon.smarthostapi.domain.api.OccupancyOptimizationResponse;
 import com.halycon.smarthostapi.service.IOptimizationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
+@Api(value = "/", description = "Optimization Insights API")
 public class InsightsController {
 
     @Resource(name = "OccupancyOptimizationService")
     private IOptimizationService<OccupancyOptimizationRequest, OccupancyOptimizationResponse> occupancyOptimizationService;
 
+    @ApiOperation(value = "Operation for getting occupancy optimization insight",
+            notes = "RoomStock criterias and test prices has to be provided together",
+            response = OccupancyOptimizationResponse.class)
     @RequestMapping(value = "/getOptimizedOccupancySummary", method = {RequestMethod.POST} , produces = "application/json")
     private OccupancyOptimizationResponse getOptimizedOccupancySummary(@RequestBody OccupancyOptimizationRequest occupancyOptimizationRequest) throws Exception{
         return occupancyOptimizationService.optimize(occupancyOptimizationRequest);
