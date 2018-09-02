6357e045f29082291ec45b436ff185a05163cd81
volkancetin <vncetin@gmail.com>
2018-09-02 20:31:24 +0300

    first test case and initial allocation written based on that.

--- src/main/java/com/halycon/smarthostapi/service/impl/OccupancyOptimizationService.java
+++ src/main/java/com/halycon/smarthostapi/service/impl/OccupancyOptimizationService.java
@@ -1,6 +1,8 @@
 package com.halycon.smarthostapi.service.impl;
 
+import com.halycon.smarthostapi.domain.RoomPriceSummary;
 import com.halycon.smarthostapi.domain.RoomStock;
+import com.halycon.smarthostapi.domain.RoomType;
 import com.halycon.smarthostapi.domain.api.OccupancyOptimizationRequest;
 import com.halycon.smarthostapi.domain.api.OccupancyOptimizationResponse;
 import com.halycon.smarthostapi.service.IOptimizationService;
@@ -8,8 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service("OccupancyOptimizationService")
 public class OccupancyOptimizationService implements IOptimizationService<OccupancyOptimizationRequest, OccupancyOptimizationResponse> {
@@ -28,10 +34,61 @@ public class OccupancyOptimizationService implements IOptimizationService<Occupa
         occupancyOptimizationRequest.getRoomStockList().sort(
                 (RoomStock r1, RoomStock r2) -> r2.getMinPrice().compareTo(r1.getMinPrice()));
 
+        Map<RoomType,RoomPriceSummary> priceSummaryMap = new HashMap<>();
+
+        occupancyOptimizationRequest.getRoomStockList().forEach(i -> priceSummaryMap.put(i.getRoomType(), new RoomPriceSummary(BigDecimal.ZERO,0,i.getRoomType())));
+
+        int startingAllocation = occupancyOptimizationRequest.getRoomStockList().get(0).getAllocation();
+        int roomTypeIndx = 0;
+
+        BigDecimal thresholdValue = occupancyOptimizationRequest.
+                getPrices()[occupancyOptimizationRequest.getPrices().length-1].add(BigDecimal.ONE);
+
+
+        for (int i = occupancyOptimizationRequest.getPrices().length-1 ; i >= 0 ; i--) {
+
+            if(roomTypeIndx > occupancyOptimizationRequest.getRoomStockList().size())
+                break;
+
+            BigDecimal price = occupancyOptimizationRequest.getPrices()[i];
+            RoomStock roomStock = occupancyOptimizationRequest.getRoomStockList().get(roomTypeIndx);
+
+            if(thresholdValue.compareTo(price)<0)
+                continue;
+
+            if(startingAllocation>0 && price.compareTo(roomStock.getMinPrice())>=0){
+                priceSummaryMap.get(roomStock.getRoomType()).incrementAllocation();
+                priceSummaryMap.get(roomStock.getRoomType()).addTotalPrice(price);
+                occupancyOptimizationRequest.getPrices()[i] = BigDecimal.ZERO;
+                startingAllocation--;
+                occupancyOptimizationRequest.getRoomStockList().get(roomTypeIndx).setAllocation(startingAllocation);
+
+                if(startingAllocation==0 || (i-1>=0 && occupancyOptimizationRequest.getPrices()[i-1].compareTo(roomStock.getMinPrice())<0 && startingAllocation>0)) {
+                    thresholdValue = roomStock.getMinPrice().subtract(BigDecimal.ONE);
+                    roomTypeIndx++;
+
+                    if(roomTypeIndx > occupancyOptimizationRequest.getRoomStockList().size()-1)
+                        break;
+
+                    startingAllocation = occupancyOptimizationRequest.getRoomStockList().get(roomTypeIndx).getAllocation();
+
+                }
+            }
+        }
+
+        response.setSuccess(true);
+
+        response.setPriceSummaries(priceSummaryMap.entrySet().stream()
+                .filter(i-> i.getValue().getAllocation()>0)
+                .map(i-> i.getValue())
+                .collect(Collectors.toList()));
+
+        logger.info(" response :: {} ", response);
+
         return response;
     }
 
-    public void validateOccupancyOptimizationRequest(OccupancyOptimizationRequest occupancyOptimizationRequest) throws Exception{
+    private void validateOccupancyOptimizationRequest(OccupancyOptimizationRequest occupancyOptimizationRequest) throws IllegalArgumentException{
         if(occupancyOptimizationRequest.getRoomStockList()==null || occupancyOptimizationRequest.getRoomStockList().size()==0)
             throw new IllegalArgumentException("RoomStock definitions missing!");
         if(occupancyOptimizationRequest.getPrices()==null || occupancyOptimizationRequest.getPrices().length==0)
@@ -40,7 +97,7 @@ public class OccupancyOptimizationService implements IOptimizationService<Occupa
         validateRoomStockDefinitions(occupancyOptimizationRequest.getRoomStockList());
     }
 
-    public void validateRoomStockDefinitions(List<RoomStock> roomStockList){
+    private void validateRoomStockDefinitions(List<RoomStock> roomStockList){
         for(RoomStock roomStock : roomStockList) {
             if(roomStock.getRoomType()==null)
                 throw new IllegalArgumentException("RoomType not defined in RoomStock :: "+roomStock.toString());
--- src/test/java/com/halycon/smarthostapi/controller/InsightsControllerTest.java
+++ src/test/java/com/halycon/smarthostapi/controller/InsightsControllerTest.java
@@ -2,19 +2,25 @@ package com.halycon.smarthostapi.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.halycon.smarthostapi.domain.RoomStock;
+import com.halycon.smarthostapi.domain.RoomType;
+import com.halycon.smarthostapi.domain.api.OccupancyOptimizationRequest;
+import com.halycon.smarthostapi.domain.api.OccupancyOptimizationResponse;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,6 +29,9 @@ public class InsightsControllerTest {
 
     @Autowired
     public RestTemplate restTemplate;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -46,5 +55,29 @@ public class InsightsControllerTest {
     public void getDemoPricesFromExternalApi_ReturnsPriceList() {
         Assert.assertNotNull("prices is not null", prices);
         Assert.assertNotEquals("prices not empty ", prices.length,0);
+    }
+
+    @Test
+    public void getOptimizedOccupancySummary_E3P3_Returns167_738() {
+        OccupancyOptimizationRequest request = new OccupancyOptimizationRequest();
+        List<RoomStock> roomStocks = new ArrayList<>();
+        roomStocks.add(new RoomStock(RoomType.ECONOMY,BigDecimal.ZERO,3));
+        roomStocks.add(new RoomStock(RoomType.PREMIUM,BigDecimal.valueOf(100),3));
+        request.setRoomStockList(roomStocks);
+        request.setPrices(prices);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        ResponseEntity<OccupancyOptimizationResponse> response = testRestTemplate.postForEntity("/getOptimizedOccupancySummary",
+                new HttpEntity<>(request, headers), OccupancyOptimizationResponse.class);
+
+        Assert.assertEquals(HttpStatus.OK,response.getStatusCode());
+        Assert.assertTrue("api response true", response.getBody().isSuccess());
+        Assert.assertNotNull("prices summaries not null", response.getBody().getPriceSummaries());
+        Assert.assertEquals("economy total price equals 167", response.getBody().getPriceSummaries().get(0).getTotalPrice(), BigDecimal.valueOf(167));
+        Assert.assertEquals("economy allocation 3", response.getBody().getPriceSummaries().get(0).getAllocation(),3);
+        Assert.assertEquals("premium total price equals 738", response.getBody().getPriceSummaries().get(1).getTotalPrice(), BigDecimal.valueOf(738));
+        Assert.assertEquals("premium allocation 3", response.getBody().getPriceSummaries().get(1).getAllocation(),3);
     }
 }
